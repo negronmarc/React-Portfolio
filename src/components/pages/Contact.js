e367a05f4c3fe09fb2d642fedcb413eda1a0139a
Marc Negron <negronmarc@gmail.com>
2022-02-04 16:49:09 -0500

    added textareas to contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
 
 export default function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
+      <form>
+        <div class="form-group col-6">
+          <label htmlFor="name">Name</label>
+          <input type="text" class="form-control" />
+        </div>
+        <div class="form-group col-6">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div class="form-group col-6">
+          <label htmlFor="message">Message</label>
+          <textarea class="form-control" rows="10"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
