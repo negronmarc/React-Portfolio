cf986d12765852feda86860a854449b168cb01fb
Marc Negron <negronmarc@gmail.com>
2022-02-20 22:04:06 -0500

    fixed hamburger menu

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,73 +1,71 @@
-import React from "react-bootstrap";
+import React from "react";
+import Bootstrap from "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function Hamburger({ currentPage, handlePageChange }) {
   return (
     <div>
-      <nav class="navbar">
+      <nav class="navbar col-right">
         <div class="container-fluid">
           <button
-            class="navbar-toggler"
+            class="navbar-toggler ms-auto btn-outline-light justify-content-between "
             type="button"
-            data-mdb-toggle="collapse"
-            data-mdb-target="#navbarToggleExternalContent"
-            aria-controls="navbarToggleExternalContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarToggleExternalContent"
           >
-            <i class="bi bi-list text-white"></i>
+            <i class="bi bi-list"></i>
           </button>
         </div>
       </nav>
       <div class="collapse" id="navbarToggleExternalContent">
-        <div class="bg-light shadow-3 p-4">
-          <button class="btn btn-link btn-block border-bottom m-0">
+        <div class="">
+          <button class="btn btn-link m-0 text-decoration-none">
             <a
               href="#about"
               onClick={() => handlePageChange("About")}
               className={
                 currentPage === "About"
-                  ? "nav-link active bg-dark"
+                  ? "nav-link active bg-dark text-white"
                   : "nav-link text-white"
               }
             >
               About Me
             </a>
           </button>
-          <button class="btn btn-link btn-block border-bottom m-0">
+          <button class="btn btn-link m-0 text-decoration-none">
             <a
               href="#portfolio"
               onClick={() => handlePageChange("Portfolio")}
               className={
                 currentPage === "Portfolio"
-                  ? "nav-link active bg-dark"
+                  ? "nav-link active bg-dark text-white"
                   : "nav-link text-white"
               }
             >
               Portfolio
             </a>
           </button>
-          <button class="btn btn-link btn-block border-bottom m-0">
+          <button class="btn btn-link m-0 text-decoration-none">
             <a
               href="#resume"
               onClick={() => handlePageChange("Resume")}
               className={
                 currentPage === "Resume"
-                  ? "nav-link active bg-dark"
+                  ? "nav-link active bg-dark text-white"
                   : "nav-link text-white"
               }
             >
               Resume
             </a>
           </button>
-          <button class="btn btn-link btn-block m-0">
+          <button class="btn btn-link btn-block m-0 text-decoration-none">
             {" "}
             <a
               href="#contact"
               onClick={() => handlePageChange("Contact")}
               className={
                 currentPage === "Contact"
-                  ? "nav-link active bg-dark"
+                  ? "nav-link active bg-dark text-white"
                   : "nav-link text-white"
               }
             >
