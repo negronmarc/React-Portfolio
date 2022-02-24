ca2975c6a3afb2537b6c0bf56c375d16ac1bcc70
Marc Negron <negronmarc@gmail.com>
2022-02-24 18:08:18 -0500

    added placeholders for contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -31,30 +31,58 @@ export default function Contact() {
       <h2 class="page-title">Contact Me</h2>
       <form ref={form} onSubmit={contactInfo}>
         <div class="text-box col-lg-6 col-sm-10 mx-auto">
-          <label htmlFor="subject" class="titles">Subject</label>
-          <input type="text" class="form-control" name="subject" />
+          <label htmlFor="subject" class="titles">
+            Subject
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            name="subject"
+            placeholder="Job Request"
+          />
         </div>
         <div class="text-box col-lg-6 col-sm-10 mx-auto">
-          <label htmlFor="name" class="titles">Name</label>
-          <input type="text" class="form-control" name="name" />
+          <label htmlFor="name" class="titles">
+            Name
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            name="name"
+            placeholder="John Smith"
+          />
         </div>
         <div class="text-box col-lg-6 col-sm-10 mx-auto">
-          <label htmlFor="email" class="titles">Email address</label>
-          <input type="email" class="form-control" name="email" />
+          <label htmlFor="email" class="titles">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            name="email"
+            placeholder="jsmith@email.com"
+          />
         </div>
         <div class="text-box col-lg-6 col-sm-10 mx-auto">
-          <label htmlFor="message" class="titles">Message</label>
-          <textarea class="form-control" rows="10" name="message"></textarea>
+          <label htmlFor="message" class="titles">
+            Message
+          </label>
+          <textarea
+            class="form-control"
+            rows="10"
+            name="message"
+            placeholder="We are building a safe and secure video community app on blockchain. We need someone to help us architect and implement a proof of concept and then (ideally) continue the project into the next phase. Contact me at this email at your earliest convenience."
+          ></textarea>
         </div>
         <div class="text-center">
-        <button
-          type="submit"
-          value="send"
-          class="btn center-block"
-          id="button"
-        >
-          Submit
-        </button>
+          <button
+            type="submit"
+            value="send"
+            class="btn center-block"
+            id="button"
+          >
+            Submit
+          </button>
         </div>
       </form>
     </div>
