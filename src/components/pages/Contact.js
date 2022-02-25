022ad584d88473d765bbec1f4a08cc2a0616ddcb
Marc Negron <negronmarc@gmail.com>
2022-02-25 05:28:00 -0500

    added modal for submit button on contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -80,9 +80,44 @@ export default function Contact() {
             value="send"
             class="btn center-block"
             id="button"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
           >
             Submit
           </button>
+
+          <div
+            class="modal fade"
+            id="staticBackdrop"
+            data-bs-backdrop="static"
+            data-bs-keyboard="false"
+            tabindex="-1"
+            aria-labelledby="staticBackdropLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-body" id="staticBackdropLabel">
+                    Email has been successfully sent!
+                  </h5>
+                </div>
+                <div class="modal-body">
+                  Thank you for getting in touch! I will respond as soon as
+                  possible.
+                </div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </form>
     </div>
